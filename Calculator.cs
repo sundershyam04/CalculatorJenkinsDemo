aeda9392b55e9d0db77d4b3ecc65c807ae4db9db
Shyam sunder@04 <sundershyam6161@gmail.com>
2022-10-21 09:43:19 +0530

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorJenkinsDemo
+{
+    public class Calculator
+    {
+
+        public int Add(int a, int b)
+        {
+
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+
+            return b != 0 ? a / b : 0;
+        }
+
+        public double Sqrroot(double a)
+        {
+
+            return a != 0 ? Math.Sqrt(a) : 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorJenkinsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calculator cal = new Calculator();
+
+            int a = 10, b = 25;
+
+            Console.WriteLine($"Addition of {a} and {b} : { cal.Add(a, b)}" );
+
+            Console.WriteLine($"Subtraction of {a} from {b} : {cal.Subtract(b, a)}");
+
+            Console.WriteLine($"Multiplication of {a} and {b} : {cal.Multiply(a, b)}");
+
+            Console.WriteLine($"Division of {b} and {a} :  {cal.Divide(b, a)}");
+
+            Console.WriteLine($"Squareroot of {b} : {cal.Sqrroot(b)}");
+      
+            Console.Read();
+        }
+    }
+}
